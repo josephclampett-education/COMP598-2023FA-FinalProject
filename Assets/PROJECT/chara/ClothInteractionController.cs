efdbf8ab424eebd0ad67461ac22691fbf4c08b2a
SakDev <saketh.devareddy1@gmail.com>
2023-12-09 16:37:31 -0500

    Added Scene Reset Capability

--- Assets/PROJECT/chara/ClothInteractionController.cs
+++ Assets/PROJECT/chara/ClothInteractionController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClothInteractionController : MonoBehaviour
 {
@@ -18,6 +19,6 @@ public class ClothInteractionController : MonoBehaviour
 
     public void OnResetState()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
